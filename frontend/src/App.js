58bceab023ded43e5bccda793f4467ea20be6182
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2022-06-20 21:09:03 -0300

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("contacts")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+  render() {
+    return (
+     <div>
+        {this.state.data.map(contacts => {
+          return (
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">State</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{contacts.first_name}</td>
+                    <td>{contacts.last_name}</td>
+                    <td>{contacts.phone}</td>
+                    <td>{contacts.email}</td>
+                    <td>{contacts.state}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const container = document.getElementById("root");
+render(<App />, container);
